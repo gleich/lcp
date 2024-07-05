c3d85119d4fa5df09c29f2e23e10ca682ae7898b
Matt Gleich <git@mattglei.ch>
2024-07-05 00:49:14 -0400

    feat: only output errors when periodically updating
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/github/update.rs
+++ src/github/update.rs
@@ -3,6 +3,7 @@ use std::time::Duration;
 use anyhow::{Context, Result};
 use reqwest::Client;
 use rocket::tokio;
+use tracing::warn;
 
 use super::{cache, repos::fetch_pinned_repos};
 
@@ -20,9 +21,15 @@ pub async fn cache(client: &Client) -> Result<()> {
 pub async fn periodic_update() -> Result<()> {
     let client = Client::new();
     loop {
-        cache(&client)
+        match cache(&client)
             .await
-            .context("requesting pinned repos or updating cache failed")?;
+            .context("requesting pinned repos or updating cache failed")
+        {
+            Ok(()) => {}
+            Err(err) => {
+                warn!("encountered error trying to update cache: {}", err)
+            }
+        }
         tokio::time::sleep(Duration::from_secs(300)).await; // reload every 5 minutes
     }
 }
--- src/steam/update.rs
+++ src/steam/update.rs
@@ -3,6 +3,7 @@ use std::time::Duration;
 use anyhow::{Context, Result};
 use reqwest::Client;
 use rocket::tokio;
+use tracing::warn;
 
 use super::{cache, games::fetch_recently_played};
 
@@ -17,9 +18,15 @@ pub async fn cache(client: &Client) -> Result<()> {
 pub async fn periodic_update() -> Result<()> {
     let client = Client::new();
     loop {
-        cache(&client)
+        match cache(&client)
             .await
-            .context("requesting recent games or updating cache failed")?;
+            .context("requesting recent games or updating cache failed")
+        {
+            Ok(()) => {}
+            Err(err) => {
+                warn!("encountered error trying to update cache: {}", err);
+            }
+        }
         tokio::time::sleep(Duration::from_secs(300)).await; // reload every 5 minutes
     }
 }
