5dc2713fb783289cd83999e51672356ca9acb269
Matt Gleich <git@mattglei.ch>
2025-01-24 15:24:03 -0500

    feat: support multiple tokens
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- internal/auth/bearer.go
+++ internal/auth/bearer.go
@@ -3,14 +3,25 @@ package auth
 import (
 	"fmt"
 	"net/http"
+	"strings"
 
 	"pkg.mattglei.ch/lcp-2/internal/secrets"
 )
 
 func IsAuthorized(w http.ResponseWriter, r *http.Request) bool {
-	if r.Header.Get("Authorization") != fmt.Sprintf("Bearer %s", secrets.ENV.ValidToken) {
-		http.Error(w, "Invalid bearer auth token", http.StatusUnauthorized)
-		return false
+	validTokens := strings.Fields(secrets.ENV.ValidToken)
+
+	givenToken := r.Header.Get("Authorization")
+	authorized := false
+	for _, token := range validTokens {
+		if givenToken != fmt.Sprintf("Bearer %s", token) {
+			authorized = true
+			break
+		}
 	}
-	return true
+
+	if !authorized {
+		http.Error(w, "Invalid bearer auth token", http.StatusUnauthorized)
+	}
+	return authorized
 }
