62cebe595211c89431ea5cff12fc014c0a4ec637
Matt Gleich <git@mattglei.ch>
2025-03-02 12:34:46 -0500

    feat: manually add timezone for hevy workouts
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- internal/apis/workouts/hevy/workouts.go
+++ internal/apis/workouts/hevy/workouts.go
@@ -47,6 +47,7 @@ func FetchWorkouts(client *http.Client) ([]lcp.Workout, error) {
 			StartDate:     workout.StartTime,
 			MovingTime:    uint32(workout.EndTime.Sub(workout.StartTime).Seconds()),
 			SportType:     "WeightTraining",
+			Timezone:      "(GMT-05:00) America/New_York",
 			HasMap:        false,
 			ID:            workout.ID,
 			HasHeartrate:  false,
