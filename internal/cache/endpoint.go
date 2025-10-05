59c767ccce9a6b61d03ba500629f5c32e21ada12
Matt Gleich <git@mattglei.ch>
2025-10-04 21:51:10 -0400

    feat: remove localhost from cors policy
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- internal/cache/endpoint.go
+++ internal/cache/endpoint.go
@@ -48,7 +48,8 @@ func (c *Cache[T]) ServeStream(w http.ResponseWriter, r *http.Request) {
 
 	origin := r.Header.Get("Origin")
 	switch origin {
-	case "http://localhost:5173", "https://mattglei.ch", "https://lcp.mattglei.ch":
+	// case "http://localhost:5173", "https://mattglei.ch", "https://lcp.mattglei.ch":
+	case "https://mattglei.ch", "https://lcp.mattglei.ch":
 		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Vary", "Origin")
 	default:
