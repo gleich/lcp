f05ff56ec9e376ed21d8f3f98351b6f13e18d452
Matt Gleich <git@mattglei.ch>
2025-12-24 12:04:59 -0500

    feat: add another playlist to sync
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- internal/apis/applemusic/playlists.go
+++ internal/apis/applemusic/playlists.go
@@ -41,14 +41,21 @@ var playlists = []lcp.AppleMusicSyncedPlaylist{
 	{Name: "house", AppleMusicID: "p.gek1EWzCLa68Adp", SpotifyID: "3iMi8ew4XvYCCcS9P2iARw"},
 	{Name: "funk", AppleMusicID: "p.O1kz7EoFVmvz704", SpotifyID: "1EDwymox6cXQlk7JGDMCbz"},
 	{Name: "old man", AppleMusicID: "p.V7VYVB0hZo53MQv", SpotifyID: "3fDlIqV43BvPvtPs9ASsgU"},
+{Name: "country", AppleMusicID: "p.O1kz7zbsVmvz704", SpotifyID: "3jR0MH0NwzEdYuUY8nohmf"},
+
+	// private playlists
 	{
-		Name:         "sleep in my arms",
+		Name:         "drifting off",
 		AppleMusicID: "p.AWXoXbWfLrvpJlY",
 		SpotifyID:    "6ex8usE1U41m4wM6NwT9cg",
 		Private:      true,
 	},
-	// {Name: "rock", AppleMusicID: "p.qQXLxpDuA75zg8e", SpotifyID: "1Yh42cCAPWPFy55hw8VaWJ"},
-	{Name: "country", AppleMusicID: "p.O1kz7zbsVmvz704", SpotifyID: "3jR0MH0NwzEdYuUY8nohmf"},
+	{
+		Name:         "x",
+		AppleMusicID: "p.PkxVx6ki2zv9xE8",
+		SpotifyID:    "66dbgoThL7wf2IYiLtyhCA",
+		Private:      true,
+	},
 }
 
 type playlistTracksResponse struct {
