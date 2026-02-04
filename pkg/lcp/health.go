b143e88208bccf6e25af0b4e98ffb6baceb1fefd
Matt Gleich <git@mattglei.ch>
2026-02-04 11:38:41 -0500

    feat: FetchHealthStatus function

--- pkg/lcp/health.go
+++ pkg/lcp/health.go
@@ -3,3 +3,11 @@ package lcp
 type HealthStatus struct {
 	Ok bool `json:"ok"`
 }
+
+func FetchHealthStatus(client *Client) (HealthStatus, error) {
+	resp, err := fetch[HealthStatus](client, "health")
+	if err != nil {
+		return HealthStatus{}, err
+	}
+	return resp, nil
+}
