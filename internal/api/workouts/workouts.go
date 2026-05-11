4cf0c150cfa58b48b715cdc018975d36248b9420
Matt Gleich <git@mattglei.ch>
2026-05-11 15:29:32 -0400

    feat: add minio integration for jellyfin image storage

--- internal/api/workouts/workouts.go
+++ internal/api/workouts/workouts.go
@@ -4,11 +4,9 @@ import (
 	"net/http"
 
 	"github.com/minio/minio-go/v7"
-	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/redis/go-redis/v9"
 	"go.mattglei.ch/lcp/internal/api/workouts/strava"
 	"go.mattglei.ch/lcp/internal/cache"
-	"go.mattglei.ch/lcp/internal/secrets"
 	"go.mattglei.ch/timber"
 )
 
@@ -16,22 +14,11 @@ const cacheInstance = cache.Workouts
 
 var logAttr = cacheInstance.LogAttr()
 
-func Setup(mux *http.ServeMux, client *http.Client, rdb *redis.Client) {
+func Setup(mux *http.ServeMux, client *http.Client, minioClient *minio.Client, rdb *redis.Client) {
 	stravaTokens := strava.LoadTokens()
 	err := stravaTokens.RefreshIfExpired(client)
 	if err != nil {
 		timber.Error(err, "failed to refresh strava token data on boot")
-	}
-	minioClient, err := minio.New(secrets.ENV.MinioEndpoint, &minio.Options{
-		Creds: credentials.NewStaticV4(
-			secrets.ENV.MinioAccessKeyID,
-			secrets.ENV.MinioSecretKey,
-			"",
-		),
-		Secure: true,
-	})
-	if err != nil {
-		timber.Fatal(err, "failed to create minio client")
 	}
 	activities, err := fetch(client, minioClient, rdb, stravaTokens)
 	if err != nil {
